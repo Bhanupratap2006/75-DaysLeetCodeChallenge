98e322eae3b2ae83cc0b10e7582ac3e7fb5a6212
Bhanupratap2006 <bhanupratap8533022407@gmail.com>
2026-07-10 12:53:44 +0530

    Time: 0 ms (100%), Space: 16.6 MB (39.47%) - LeetHub

---
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* first = new ListNode(0);
+        first->next = head;
+        head = first;
+
+        while (first->next) {
+
+            ListNode* temp = first;
+            for (int i = 0; i < k; i++) {
+                temp = temp->next;
+                if (temp == NULL) {
+                    return head->next;
+                }
+            }
+
+            int x = k;
+            ListNode* second = first->next;
+            ListNode* prev = first;
+            ListNode* curr = first->next;
+
+            while (x && curr) {
+                ListNode* front = curr->next;
+                curr->next = prev;
+                prev = curr;
+                curr = front;
+                x--;
+            }
+
+            first->next = prev;
+            second->next = curr;
+            first = second;
+        }
+
+        first = head;
+        head = head->next;
+        delete first;
+
+        return head;
+    }
+};
