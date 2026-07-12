3ddac2107c5f6495145462afda104f2c60dc5761
Bhanupratap2006 <bhanupratap8533022407@gmail.com>
2026-07-12 07:50:24 +0530

    Time: 6 ms (54.75%), Space: 14.9 MB (86.21%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -1,11 +1,11 @@
 /*
-// Definition for a Node.
+ // Definition for a Node.
 class Node {
 public:
     int val;
     Node* next;
     Node* random;
-    
+
     Node(int _val) {
         val = _val;
         next = NULL;
@@ -16,42 +16,64 @@ public:
 
 class Solution {
 public:
-
     Node* copyRandomList(Node* head) {
-        Node*headcopy=new Node(0);
-        Node*tailcopy=headcopy;
-        Node*temp=head;
-         while(temp)
-         {
-            tailcopy->next=new Node(temp->val);
-            tailcopy=tailcopy->next;
-            temp=temp->next;
-         }
-         tailcopy=headcopy;
-         headcopy=headcopy->next;
-         delete tailcopy;
 
-         tailcopy=headcopy;
-         temp=head;
-         unordered_map<Node*,Node*>m;
+        if (head == NULL)
+            return NULL;
 
-         while(temp){
-            m[temp]=tailcopy;
-            tailcopy=tailcopy->next;
-            temp=temp->next;
-            
+        Node* headcopy = new Node(0);
+        Node* tailcopy = headcopy;
+        Node* temp = head;
 
-         }
-         tailcopy=headcopy;
-            temp=head;
-         while(temp){
-            tailcopy->random=m[temp->random];
-            tailcopy=tailcopy->next;
-            temp=temp->next;
-            
-         }
-         return headcopy;
+        while (temp) {
+            tailcopy->next = new Node(temp->val);
+            tailcopy = tailcopy->next;
+            temp = temp->next;
+        }
 
-        
+        tailcopy = headcopy;
+        headcopy = headcopy->next;
+        delete tailcopy;
+
+        Node* curr1 = head;
+        Node* curr2 = headcopy;
+        Node* front1, *front2;
+
+        while (curr1) {
+            front1 = curr1->next;
+            front2 = curr2->next;
+
+            curr1->next = curr2;
+            curr2->next = front1;
+
+            curr1 = front1;
+            curr2 = front2;
+        }
+
+        curr1 = head;
+
+        while (curr1) {
+            curr2 = curr1->next;
+
+            if (curr1->random)
+                curr2->random = curr1->random->next;
+
+            curr1 = curr2->next;
+        }
+
+        curr1 = head;
+        curr2 = headcopy;
+
+        while (curr1 && curr2) {
+            curr1->next = curr2->next;
+            curr1 = curr1->next;
+
+            if (curr1) {
+                curr2->next = curr1->next;
+                curr2 = curr2->next;
+            }
+        }
+
+        return headcopy;
     }
 };
