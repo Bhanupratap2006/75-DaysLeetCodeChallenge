0cbb817c0b1550e58164c9da916b6b6b4781da35
Bhanupratap2006 <bhanupratap8533022407@gmail.com>
2026-07-15 01:16:12 +0530

    Time: 20 ms (53.93%), Space: 58.3 MB (54.78%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -8,29 +8,63 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
 public:
-    ListNode* sortList(ListNode* head) {
-        vector<int>arr;
-        ListNode*curr=head;
-        if(head == NULL)
-        return NULL;
-        while(curr)
+
+    ListNode* findmid(ListNode* head)
+    {
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while(fast && fast->next)
         {
-            arr.push_back(curr->val);
-            curr=curr->next;
-
-        }
-        sort(arr.begin(),arr.end());
-        curr = head;
-        for (int i = 0; i < arr.size(); i++) {
-            curr->val = arr[i];
-            curr = curr->next;
+            slow = slow->next;
+            fast = fast->next->next;
         }
 
-        return head;
-        
-        
-        
+        return slow;
+    }
+
+    ListNode* merge(ListNode* left, ListNode* right)
+    {
+        if(left == NULL)
+            return right;
+
+        if(right == NULL)
+            return left;
+
+        if(left->val <= right->val)
+        {
+            left->next = merge(left->next, right);
+            return left;
+        }
+        else
+        {
+            right->next = merge(left, right->next);
+            return right;
+        }
+    }
+
+    ListNode* sortList(ListNode* head)
+    {
+        // Base Case
+        if(head == NULL || head->next == NULL)
+            return head;
+
+        // Find Middle
+        ListNode* mid = findmid(head);
+
+        // Split List
+        ListNode* left = head;
+        ListNode* right = mid->next;
+        mid->next = NULL;
+
+        // Sort Left and Right
+        left = sortList(left);
+        right = sortList(right);
+
+        // Merge
+        return merge(left, right);
     }
 };
