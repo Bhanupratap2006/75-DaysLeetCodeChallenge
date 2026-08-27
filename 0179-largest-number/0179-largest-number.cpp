be7755552574c489ea58756fe2228ab04b5f7053
Bhanupratap2006 <bhanupratap8533022407@gmail.com>
2026-08-28 00:02:11 +0530

    Time: 3 ms (74.22%), Space: 17 MB (56.73%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        vector<string> s;
+
+        for (int i = 0; i < nums.size(); i++) {
+            s.push_back(to_string(nums[i]));
+        }
+
+        sort(s.begin(), s.end(), [](string a, string b) {
+            return a + b > b + a;
+        });
+
+        if (s[0] == "0")
+            return "0";
+
+        string ans = "";
+
+        for (int i = 0; i < s.size(); i++) {
+            ans += s[i];
+        }
+
+        return ans;
+    }
+};
